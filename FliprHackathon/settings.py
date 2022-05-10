3194d99bca348ca9da9e5b4bccbaf98b06cd92d5
shivamgopchade <shivam23gopchade.com>
2022-05-10 21:53:49 +0530

    commits

--- FliprHackathon/settings.py
+++ FliprHackathon/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -135,3 +135,5 @@ LOGIN_REDIRECT_URL='home'
 
 LOGIN_URL='login'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
